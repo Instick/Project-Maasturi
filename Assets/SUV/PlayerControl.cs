1639e4f32d61128d1c0f2678964dee46364f6509
t3laja00 <t3laja00@students.oamk.fi>
2015-11-20 12:26:34 +0200

    Tekstuurit lisätty

--- Assets/SUV/PlayerControl.cs
+++ Assets/SUV/PlayerControl.cs
@@ -1,28 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour
-{
-    public float speed;
-
-
-    // Use this for initialization
-    void Start () {
+public class PlayerControl : MonoBehaviour {
 	
+
+	
+	// Use this for initialization
+	void FixedUpdate () {
+		
+
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-    void FixedUpdate()
-    {
-        float moveVertical;
-        float moveHorizontal;
-        moveVertical = Input.GetAxis("Vertical");
-        moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
-    }
+
+}
+
+		
+		
 
 }
