fae441e0d0ddb89c8fbb6b71ad42c230fd19ae7f
t3laja00 <t3laja00@students.oamk.fi>
2015-11-23 10:21:19 +0200

    *Lisätty kerättävät objektit

---
+++ Assets/Spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+	public float speed = 25f;
+	
+	
+	void Update ()
+	{
+
+		transform.Rotate(0, speed * Time.deltaTime, 0, Space.World);
+	}
+}
