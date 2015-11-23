7ec7341d3621a2ee522fc900eebdc170c0e4d727
unknown <esa.heinonen@DTFI-252.deetee.com>
2015-11-23 21:04:39 +0200

    Toimiva auto

--- Assets/SUV/PlayerControl.cs
+++ Assets/SUV/PlayerControl.cs
@@ -1,70 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerControl: MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
 
-    public WheelCollider FrontLeft;
-    public WheelCollider FrontRight;
-    public WheelCollider RearRight;
-    public WheelCollider RearLeft;
-
-    float speed = 30.0f;
-    float braking = 20.0f;
-    float turning = 20.0f;
-
-    // Use this for initialization
-    void Start()
+    public void FixedUpdate()
     {
-        GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -1, 0);
-       // rigidbody.centerOfMass = new Vector3(0, -1, 0);
-    }
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-    // Update is called once per frame
-    void Update()
-     {
-
-        //this code makes car go forward
-        if (Input.GetKey(KeyCode.A))
+        foreach (AxleInfo axleInfo in axleInfos)
         {
-            RearRight.motorTorque = Input.GetAxis("Vertical") * speed;
-            RearLeft.motorTorque = Input.GetAxis("Vertical") * speed;
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
         }
-
-        if (Input.GetKey("up"))
-        {
-            //Debug.Log("up painettu");
-            RearRight.motorTorque = Input.GetAxis("Vertical") * speed;
-            RearLeft.motorTorque = Input.GetAxis("Vertical") * speed;
-
-            //Debug.Log("Vääntö: " + RearRight.motorTorque);
-
-        }
-
-
-        //this code is for turning
-        if (Input.GetKey("right"))
-          {
-              FrontRight.steerAngle = Input.GetAxis("Horizontal") * turning;
-              FrontLeft.steerAngle = Input.GetAxis("Horizontal") * turning;
-          }
-
-          if (Input.GetKey("left"))
-          {
-              FrontRight.steerAngle = Input.GetAxis("Horizontal") * turning;
-              FrontLeft.steerAngle = Input.GetAxis("Horizontal") * turning;
-          }
-
-          //Breaking
-          if (Input.GetKey("down"))
-          {
-              RearRight.brakeTorque = braking;
-              RearLeft.brakeTorque = braking;
-          }
-
-        RearRight.brakeTorque = 0;
-        RearLeft.brakeTorque = 0;
-
     }
+}
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
 }
